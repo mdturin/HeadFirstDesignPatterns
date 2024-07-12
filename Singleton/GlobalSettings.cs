075b88290c07a8d65ef7d81c28458d1e7261fb8e
Md Turin <matkhan01@yahoo.com>
2024-07-13 04:00:36 +0600

    singleton done

---
+++ Singleton/GlobalSettings.cs
@@ -0,0 +1,20 @@
+﻿namespace Singleton;
+
+public class GlobalSettings
+{
+    private GlobalSettings() { }
+    private static GlobalSettings? _instance;
+    private static readonly object _lock = new();
+    public static GlobalSettings GetInstance()
+    {
+        if(_instance == null)
+        {
+            lock (_lock)
+            {
+                _instance ??= new GlobalSettings();
+            }
+        }
+
+        return _instance;
+    }
+}
