b22608c63e61bfec7def33c0be370f29fe77863b
Md Turin <matkhan01@yahoo.com>
2024-07-13 23:46:11 +0600

    two example added

---
+++ ConfigurationManagementSystem/Program.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+
+namespace ConfigurationManagementSystem;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var settings = Settings.GetInstance();
+        Console.WriteLine(settings.GetValue<string>("Name"));
+    }
+}
+
+public class Settings
+{
+    private readonly object _lock = new();
+    private Dictionary<string, object> AppSettings { get; set; }
+    private const string _appSettingsFileName = "AppSettings.json";
+    private static readonly Lazy<Settings> _instance
+        = new(() => new Settings());
+
+    private Settings() => Load(false);
+    
+    private void Load(bool reload)
+    {
+        try
+        {
+            using StreamReader sr = new(_appSettingsFileName);
+            AppSettings = JsonConvert
+                .DeserializeObject<Dictionary<string, object>>(sr.ReadToEnd())!;
+        }
+        catch (Exception ex)
+        {
+            if(!reload) AppSettings = [];
+            Console.WriteLine($"Error reading {_appSettingsFileName}: {ex.Message}");
+        }
+    }
+
+    public static Settings GetInstance() => _instance.Value;
+    
+    public T? GetValue<T>(string key)
+    {
+        lock (_lock)
+        {
+            if (!AppSettings.ContainsKey(key))
+                return default;
+
+            AppSettings.TryGetValue(key, out var value);
+            return value is T _value ? _value : default;
+        }
+    }
+
+    public void SetValue(string key, object value)
+    {
+        lock (_lock)
+        {
+            if (AppSettings.TryAdd(key, value))
+                AppSettings[key] = value;
+        }
+    }
+
+    public void Reload()
+    {
+        lock (_lock)
+        {
+            Load(true);
+        }
+    }
+}
---
+++ PaymentGatway/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Reflection;
+
+namespace PaymentGateway;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new PaymentFactory();
+        var payPal = factory
+            .CreatePaymentMethod(PaymentType.PayPal);
+        payPal.Transfer(1000);
+    }
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class PaymentAttribute : Attribute
+{
+    public PaymentType Type { get; set; }
+}
+
+public interface IPayment
+{
+    void Transfer(double value);
+}
+
+[Payment(Type = PaymentType.CreditCard)]
+public class CreditCard : IPayment
+{
+    public void Transfer(double value)
+    {
+        Console.WriteLine($"Paying {value} from credit card");
+    }
+}
+
+[Payment(Type = PaymentType.PayPal)]
+public class PayPal : IPayment
+{
+    public void Transfer(double value)
+    {
+        Console.WriteLine($"Paying {value} from pay pal");
+    }
+}
+
+[Payment(Type = PaymentType.BankTransfer)]
+public class BankTransfer: IPayment
+{
+    public void Transfer(double value)
+    {
+        Console.WriteLine($"Paying {value} from bank");
+    }
+}
+
+public enum PaymentType
+{
+    CreditCard,
+    PayPal,
+    BankTransfer
+}
+
+public class PaymentFactory
+{
+    private readonly Dictionary<PaymentType, Type> _types = [];
+    public static List<Type> GetImplementationsOfInterface<T>()
+    {
+        var interfaceType = typeof(T);
+        var assembly = Assembly.GetExecutingAssembly();
+        return assembly.GetTypes()
+            .Where(t => interfaceType.IsAssignableFrom(t) && t.IsClass)
+            .ToList();
+    }
+
+    public PaymentFactory()
+    {
+        var implementations = GetImplementationsOfInterface<IPayment>();
+        foreach(var implementation in implementations)
+        {
+            var attr = implementation.GetCustomAttribute<PaymentAttribute>();
+            if(attr != null)
+                _types.TryAdd(attr.Type, implementation);
+        }
+    }
+
+    public IPayment CreatePaymentMethod(PaymentType type)
+    {
+        if(_types.TryGetValue(type, out var implementation))
+        {
+            var instance = Activator.CreateInstance(implementation);
+            if (instance is IPayment _instance)
+                return _instance;
+
+            throw new Exception($"Couldn't create object of type: {instance}");
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(type), type, "Invalid payment type");
+    }
+}
