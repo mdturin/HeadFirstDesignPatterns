1e27be60a1c83719b0b6f4db038f54db5eca9a1a
Md Turin <matkhan01@yahoo.com>
2024-07-15 00:15:24 +0600

    first phase

---
+++ MenuItems/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DinerAndPancake;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class MenuItem(
+    string name, double price, bool vegetarian, string description)
+{
+    private string Name { get; set; } = name;
+    private double Price { get; set; } = price;
+    private bool Vegetarian { get; set; } = vegetarian;
+    private string Description { get; set; } = description;
+
+    public string GetName() => Name;
+    public double GetPrice() => Price;
+    public bool IsVegetarian() => Vegetarian;
+    public string GetDescription() => Description;
+}
