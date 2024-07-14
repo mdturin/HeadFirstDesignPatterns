45a8691524ae3cb2791d51d7f9c48315e36bfbd6
Md Turin <matkhan01@yahoo.com>
2024-07-15 00:24:31 +0600

    PancakeHouseMenu added

--- MenuItems/Program.cs
+++ MenuItems/Program.cs
@@ -9,7 +9,7 @@ internal class Program
 }
 
 public class MenuItem(
-    string name, double price, bool vegetarian, string description)
+    string name, string description, bool vegetarian, double price)
 {
     private string Name { get; set; } = name;
     private double Price { get; set; } = price;
@@ -20,4 +20,40 @@ public class MenuItem(
     public double GetPrice() => Price;
     public bool IsVegetarian() => Vegetarian;
     public string GetDescription() => Description;
+}
+
+public class PancakeHouseMenu
+{
+    private List<MenuItem> MenuItems { get; set; } = [];
+    public PancakeHouseMenu()
+    {
+        AddItem("K&B's Pancake Breakfast",
+                "Pancakes with scrambled eggs and toast",
+                true,
+                2.99);
+
+        AddItem("Regular Pancake Breakfast",
+                "Pancakes with fried eggs, sausage",
+                false,
+                2.99);
+
+        AddItem("Blueberry Pancakes",
+                "Pancakes made with fresh blueberries",
+                true,
+                3.49);
+
+        AddItem("Waffles",
+                "Waffles with your choice of blueberries or strawberries",
+                true,
+                3.59);
+    }
+
+    public void AddItem(
+        string name, string description, bool vegetarian, double price)
+    {
+        MenuItems
+            .Add(new MenuItem(name, description, vegetarian, price));
+    }
+
+    public List<MenuItem> GetMenuItems() => MenuItems;
 }
