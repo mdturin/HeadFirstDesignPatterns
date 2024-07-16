40f3caf818d25f53281eeb2f451371bc5248acbe
Md Turin <matkhan01@yahoo.com>
2024-07-16 23:46:50 +0600

    state pattern added

---
+++ MightyGumball/Program.cs
@@ -0,0 +1,170 @@
+﻿namespace MightyGumball;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public enum GumballState : short
+{
+    Sold_Out,
+    No_Quater,
+    Has_Quarter,
+    Sold
+}
+
+public interface IState
+{
+    void InsertQuarter();
+    void EjectQuarter();
+    void TurnCrank();
+    void Dispense();
+}
+
+public class NoQuarterState(GumballMachine machine) : IState
+{
+    private readonly GumballMachine _machine = machine;
+
+    public void Dispense()
+        => Console.WriteLine("You need to pay first");
+
+    public void EjectQuarter()
+        => Console.WriteLine("You haven't inserted a quarter");
+
+    public void InsertQuarter()
+    {
+        Console.WriteLine("You inserted a quarter");
+        _machine.SetState(GumballState.Has_Quarter);
+    }
+
+    public void TurnCrank()
+        => Console.WriteLine("You turned, but there's no quarter");
+}
+
+public class HasQuarterState(GumballMachine machine) : IState
+{
+    private readonly GumballMachine _machine = machine;
+
+    public void Dispense()
+    {
+        Console.WriteLine("No gumball dispensed");
+    }
+
+    public void EjectQuarter()
+    {
+        Console.WriteLine("Quarter returned");
+        _machine.SetState(GumballState.No_Quater);
+    }
+
+    public void InsertQuarter()
+    {
+        Console.WriteLine("You can't insert another quarter");
+    }
+
+    public void TurnCrank()
+    {
+        Console.WriteLine("You turned...");
+        _machine.SetState(GumballState.Sold);
+    }
+}
+
+public class SolidState(GumballMachine machine) : IState
+{
+    private readonly GumballMachine _machine = machine;
+
+    public void Dispense()
+    {
+        _machine.ReleaseBall();
+        if(_machine.GetCount() > 0)
+        {
+            _machine.SetState(GumballState.No_Quater);
+        }
+        else
+        {
+            Console.WriteLine("Oops, out of gumballs!");
+            _machine.SetState(GumballState.Sold_Out);
+        }
+    }
+
+    public void EjectQuarter()
+    {
+        Console.WriteLine("Sorry, you already turned the crank");
+    }
+
+    public void InsertQuarter()
+    {
+        Console.WriteLine("Please wait, we're already giving you a gumball");
+    }
+
+    public void TurnCrank()
+    {
+        Console.WriteLine("Turning twice doesn't get you another gumball!");
+    }
+}
+
+public class SolidOutState(GumballMachine machine) : IState
+{
+    private readonly GumballMachine _machine = machine;
+
+    public void Dispense()
+    {
+        Console.WriteLine("No gumball dispensed");
+    }
+
+    public void EjectQuarter()
+    {
+        Console.WriteLine("You can't eject, you haven't inserted a quarter yet");
+    }
+
+    public void InsertQuarter()
+    {
+        Console.WriteLine("You can't insert a quarter, the machine is sold out");
+    }
+
+    public void TurnCrank()
+    {
+        Console.WriteLine("You turned, but there are no gumballs");
+    }
+}
+
+public class GumballMachine
+{
+    private int _count = 0;
+    private GumballState State { get; set; }
+    private readonly Dictionary<GumballState, IState> _states = [];
+
+    public GumballMachine(int numberGumballs)
+    {
+        _count = numberGumballs;
+        _states.Add(GumballState.No_Quater, new NoQuarterState(this));
+        _states.Add(GumballState.Has_Quarter, new HasQuarterState(this));
+        _states.Add(GumballState.Sold, new SolidState(this));
+        _states.Add(GumballState.Sold_Out, new SolidOutState(this));
+    }
+
+    public void SetState(GumballState state)
+        => State = state;
+
+    public void InsertQuarter() => _states[State].InsertQuarter();
+
+    public void EjectQuarter() => _states[State].EjectQuarter();
+
+    public void TurnCrank()
+    {
+        _states[State].TurnCrank();
+        _states[State].Dispense();
+    }
+
+    public void ReleaseBall()
+    {
+        Console.WriteLine(
+            "A gumball comes rolling out the slot...");
+        if (_count > 0) _count = _count - 1;
+    }
+
+    public int GetCount() => _count;
+}
+
